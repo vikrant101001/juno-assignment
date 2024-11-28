de55aa9c154de506b3af9489b395d204119131fa
vikrant101001 <71218338+vikrant101001@users.noreply.github.com>
2024-11-28 16:12:49 +0530

    Update vite.config.js

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: process.env.PORT || 80, // Render will assign a port, fallback to 80
+    host: '0.0.0.0', // Make sure it listens on all interfaces
+  }
 })
