6e9071e7216de6c479e4bd05cc09451351228178
vikrant101001 <71218338+vikrant101001@users.noreply.github.com>
2024-11-28 16:36:48 +0530

    Update main.ts

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -13,7 +13,10 @@ async function bootstrap() {
   
   // Enable CORS for a specific origin
   app.enableCors({
-    origin: 'http://localhost:5173', // The frontend URL you want to allow
+    origin: [
+      'http://localhost:5173',  // Local frontend URL
+      'https://juno-assignment-frontend.onrender.com',  // Deployed frontend URL
+    ],
     methods: ['GET', 'POST', 'PUT', 'DELETE','PATCH',], // Allowed methods
     credentials: true, // If you need to send cookies or authentication headers
   });
